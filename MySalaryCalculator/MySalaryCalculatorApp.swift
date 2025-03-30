2fcedafe499d96832700208bad17315be41513a0
Theordius <87307483+Theordius@users.noreply.github.com>
2025-03-30 14:16:53 +0200

    Initial Commit

---
+++ MySalaryCalculator/MySalaryCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySalaryCalculatorApp.swift
+//  MySalaryCalculator
+//
+//  Created by Rafał Gęsior on 30/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct MySalaryCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
